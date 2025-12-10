04b305903db751c7890f1f118c60f483e96ab6d7
AidanRicks <aidanricks@u.boisestate.edu>
2025-12-09 22:19:43 -0700

    adding AR
    AR

---
+++ Assets/Scripts/AR_Scripts/_rockDebug.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class _rockDebug : MonoBehaviour
+{
+    //debug to know that the image was seen by vuforia
+    void Start()
+    {
+        Debug.Log("Vuforia image target recognized");
+    }
+
+}

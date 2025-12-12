29fab10228db4b44554c017a253be85a8acb6105
AidanRicks <aidanricks@u.boisestate.edu>
2025-12-11 22:02:27 -0700

    adding AR ctivation of ULT

--- Assets/Scripts/ArrowPlayerAttack.cs
+++ Assets/Scripts/ArrowPlayerAttack.cs
@@ -112,7 +112,7 @@ public class ArrowPlayerAttack : MonoBehaviour
         Gizmos.DrawWireCube(castCenter, castSize);
     }
 
-    private void FireProjectile()
+    public void FireProjectile()
     {
         GameObject newProjectile = FindProjectile();
         if (newProjectile != null)
